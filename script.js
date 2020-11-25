33aeab065bb0ee758987305eefa34fa582385df1
Yuliya Babitskaya <bambina-kissi@yandex.ru>
2020-11-25 15:16:14 +0300

    add task lesson05

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 'use strict';
 
-function addString(str){
-
-    if(typeof(str) === 'string'){
-        let string = str.trim();
-        if(string.length > 30){
-            return string.slice(0, 30) + '...';
-        }
-    } else{
-        return 'Передана не строка';
+let arr = ['155', '264', '458', '459', '234', '577', '725'];
+let newArr = [];
+for (let i = 0; i < arr.length; i++) {
+    let num = Number(arr[i].slice(0, 1));
+    if(num === 2 || num === 4){
+        newArr.push(arr[i]);
+        console.log(newArr);
     }
 }
-console.log(addString(' Как удалить пробелы? Для JavaScript такой проблемы не существует - все очень просто!  '));
+
+
+let arrPrime = [];
+
+for (let i = 2; i <= 100; i++) {
+    arrPrime = [];
+    let j = 1;
+    do {
+        let res = i % j;
+        arrPrime.push(res);
+        j++;
+    } while( j <= i);
+    let arrNum = arr.filter(item => item === 0);
+    if(arrNum.length <= 2){
+        console.log(i, `Делители этого числа: 1 и ${i}`);
+    }
+}
