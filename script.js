7b7e4d9d2681a97809893e8d365831dbe8f960b9
Yuliya Babitskaya <bambina-kissi@yandex.ru>
2020-11-26 15:49:42 +0300

    add task Lesson6

--- script.js
+++ script.js
@@ -1,28 +1,51 @@
 'use strict';
 
-let arr = ['155', '264', '458', '459', '234', '577', '725'];
-let newArr = [];
-for (let i = 0; i < arr.length; i++) {
-    let num = Number(arr[i].slice(0, 1));
-    if(num === 2 || num === 4){
-        newArr.push(arr[i]);
-        console.log(newArr);
+let current = 4;
+
+function attemptCount(count){
+    console.log(count);
+     if(count <= 0){
+        let start = confirm('Попытки закончились, хотите сыграть еще?');
+        if(start){
+            current = 4;
+            guessNumber();
+            console.log(start);
+        } else {
+            console.log(start);
+        }
+    } else{
+        guessNumber();
     }
 }
 
 
-let arrPrime = [];
-
-for (let i = 2; i <= 100; i++) {
-    arrPrime = [];
-    let j = 1;
-    do {
-        let res = i % j;
-        arrPrime.push(res);
-        j++;
-    } while( j <= i);
-    let arrNum = arr.filter(item => item === 0);
-    if(arrNum.length <= 2){
-        console.log(i, `Делители этого числа: 1 и ${i}`);
+function guessNumber() {
+    let numGame = 5;
+    let numUser = +prompt('Угадай число от 1 до 100');
+    if(numUser > 0 && numUser < numGame) {
+        current--;
+        alert('Загаданное число меньше, осталось попыток ' + current);
+        attemptCount(current);
+    } else if (numUser > numGame) {
+        current--;
+        alert('Загаданное число больше, осталось попыток ' + current);
+        attemptCount(current);
+    } else if (isNaN(numUser)) {
+        current--;
+        alert('Введи число! осталось попыток ' + current);
+        guessNumber();
+    } else if (numUser === 0) {
+        alert('Игра окончена');
+    } else if (numUser === numGame) {
+        let finish = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
+        if(finish){
+            current = 4;
+            guessNumber();
+            
+        } else {
+            return;
+        }
     }
 }
+
+guessNumber();
