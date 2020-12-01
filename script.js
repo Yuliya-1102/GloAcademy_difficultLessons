0d535e6cca6257ac0bc5b499f9a977f7e30b6da0
Yuliya Babitskaya <bambina-kissi@yandex.ru>
2020-12-01 20:48:38 +0300

    add task Lesson9

--- script.js
+++ script.js
@@ -1,25 +1,54 @@
 'use strict';
 
-let weekDays = document.querySelector('.weekDays');
-let week = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
-let date = new Date();
+const showOne = document.querySelector('.showOne');
+const showTwo = document.querySelector('.showTwo');
 
-date.setDate(date.getDate() -1);
-let currentDay = date.getDay();
+function showDays(){
+  let monthArr = ["января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря"];
+  let weekArr = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
 
-function addWeekDays(){
-  for(let i = 0; i < week.length; i++){
-    let p = document.createElement('p');
-    p.innerHTML = week[i];
-    if(i === 5 || i === 6){
-      p.style.fontStyle = 'italic';
-      if(i === currentDay){
-        p.style.fontWeight = 'bold';
-      }
-    } else if(i === currentDay){
-      p.style.fontWeight = 'bold';
-    } 
-    weekDays.append(p);
+
+  let date = new Date();
+
+  let year = date.getFullYear();
+  let dateCurrent = date.getDate();
+  let month = date.getMonth();
+
+  date.setDate(date.getDate() -1);
+  let dayCurrent = weekArr[date.getDay()];
+
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+
+  function getMonth(){
+    if(month === 0){
+      return 1;
+    } else {
+      return month + 1;
+    }
   }
-}  
-addWeekDays();
+  getMonth();
+
+  function getHoursName(hours){
+    if(hours === 1 || hours === 21){
+      return 'час';
+    } else if((hours >= 2 && hours <= 3) || (hours >= 22 && hours <= 24)){
+      return 'часа';
+    } else if((hours >= 5 && hours <= 20)){
+      return 'часов';
+    }
+  }
+  getHoursName(hours);
+
+  const addZero = n => n < 10 ? '0' + n : n;
+
+  let currentDayOne = (`Сегодня ${dayCurrent}, ${dateCurrent} ${monthArr[month]} ${year} года, ${addZero(hours)} ${getHoursName(hours)} ${addZero(minutes)} минуты ${addZero(seconds)} секунды`);
+  let currentDayTwo =  (`${addZero(dateCurrent)}.${addZero(getMonth())}.${year} - ${addZero(hours)}:${addZero(minutes)}:${addZero(seconds)}`);
+
+  showOne.innerHTML = currentDayOne;
+  showTwo.innerHTML = currentDayTwo;
+
+}
+showDays();
+setInterval(showDays, 1000);
